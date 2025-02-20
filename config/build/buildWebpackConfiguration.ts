003fceb7e31eca54dcc5ae2ef7f2fbe87a21ee3f
Vlad <142720001+StinflixWork@users.noreply.github.com>
2025-02-20 23:20:36 +0200

    Split config into modules

---
+++ config/build/buildWebpackConfiguration.ts
@@ -0,0 +1,24 @@
+import webpack from "webpack";
+import {buildLoaders} from "./buildLoaders";
+import {buildPlugins} from "./buildPlugins";
+import {buildResolvers} from "./buildResolvers";
+import {BuildOptions} from "./types/config";
+
+export function buildWebpackConfiguration(options: BuildOptions): webpack.Configuration {
+    const {mode, paths} = options;
+
+    return {
+        mode: mode,
+        entry: paths.entry,
+        output: {
+            filename: '[name].js',
+            path: paths.build,
+            clean: true
+        },
+        plugins: buildPlugins(options),
+        module: {
+            rules: buildLoaders()
+        },
+        resolve: buildResolvers(),
+    }
+}
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,33 +1,21 @@
-import path from 'path'
 import webpack from 'webpack'
-import HtmlWebpackPlugin from 'html-webpack-plugin'
+import {buildWebpackConfiguration} from "./config/build/buildWebpackConfiguration";
+import {BuildPath} from "./config/build/types/config";
+import path from "path";
 
-const config: webpack.Configuration = {
-  mode: 'production',
+const paths: BuildPath = {
   entry: path.resolve(__dirname, 'src', 'index.ts'),
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ]
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true
-  },
-  plugins: [
-    new webpack.ProgressPlugin(),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public', 'index.html')
-    }),
-  ]
+  build: path.resolve(__dirname, 'dist'),
+  html: path.resolve(__dirname, 'public', 'index.html'),
 }
+
+const mode = 'development'
+const isDev = mode === 'development'
+
+const config: webpack.Configuration = buildWebpackConfiguration({
+  mode: 'production',
+  paths,
+  isDev
+})
 
 export default config;
